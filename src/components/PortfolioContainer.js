226431a096311d4d6520ad876bff304c3c95bb7a
Joaquin Salazar <joaqsala@gmail.com>
2023-06-22 15:51:58 -0500

    added code/components to PortfolioContainer file

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,39 @@
 import React, { useState }  from 'react';
+import NavTabs from './NavTabs';
+import Home from './pages/Home';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 export default function PortfolioContainer() {
     const [currentPage, setCurrentPage] = useState('Home');
 
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About me') {
+      return <About />;
+    }
+    if (currentPage === 'Portfolio') {
+        return <Portfolio />;
+      }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    return <Resume />;
+  };
 
+  const handlePageChange = (page) => setCurrentPage(page);
 
     return (
         <div>
-          "something"
+        {/* Passing the currentPage from state and the function to update it */}
+        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+        {/* Calling the renderPage method which will return a component  */}
+        {renderPage()}
         </div>
       );
 };
