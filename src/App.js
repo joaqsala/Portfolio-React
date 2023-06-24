f23fa2e81c2ef17c23dd874cef0e7319829fc28c
Joaquin Salazar <joaqsala@gmail.com>
2023-06-24 18:57:28 -0500

    changed App.js to have 3 components instead of one main one

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import React from 'react';
 
-const App = () => <PortfolioContainer />;
+import PortfolioContainer from './components/PortfolioContainer';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <PortfolioContainer />
+        </div>
+        <Footer />
+      </div>
+  );
+}
 
 export default App;
