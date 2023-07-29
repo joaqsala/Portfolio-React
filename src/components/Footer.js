4ae1434047035d887417e9ff9194bf5b9b818ac7
Joaquin Salazar <joaqsala@gmail.com>
2023-07-29 15:03:35 -0500

    added content for the footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { FaLinkedinIn, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 // TODO: Will need to add emoji's for LinkedIn, Github, and Twitter (also, start twitter account)
 function Footer() {
     return (
-        <div className="bg-gradient-to-r from-slate-300 to-slate-400">
-            <div className='icons text-center'>
-            <i className="FaGithub"></i>
-        </div>
+        <div className="bg-gradient-to-r from-slate-300 to-slate-400 p-6">
+            <div className="flex justify-center space-x-10">
+                <a href="https://www.linkedin.com/in/joaqsala/"
+                    className=""
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FaLinkedin className="text-5xl mx-8"/>
+                </a>
+                <a href="https://github.com/joaqsala"
+                    className=""
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <FaGithub className="text-5xl mx-8"/>
+                </a>
+            </div>
         </div>
     );
 }
