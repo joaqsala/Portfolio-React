163f5797301c99e67889338ab99095f1a6cc519b
Joaquin Salazar <joaqsala@gmail.com>
2023-07-29 22:44:27 -0500

    added project info and screenshots

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import Navigation from '../components/Navigation';
 function Header() {
 
   return (
-    <nav className="header h-16 m-5 flex items-center">
+    <nav className="header h-12 m-5 flex items-center">
       <div className="container mx-auto flex flex-col sm:flex-row justify-between items-center">
         <div className="logo text-3xl ml-5 mb-2 sm:mb-0">
           JOAQSALA
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -3,9 +3,19 @@ import React from 'react';
 // TODO: Need to add projects/layouts for Portfolio
 export default function Portfolio() {
     return (
-        <div>
-            <h1>Portfolio</h1>
-            <p>Include images to group projects et al</p>
+        <div >
+            <h1 className="text-2xl mb-10">Portfolio</h1>
+        
+            <div class="row h-100 align-items-center">
+                <p>
+                    Ny name is Joaquin. I'm a mathematics content specialist transitioning into the world of programming.
+
+                    I recently completed an intensive coding bootcamp through the University of Texas at Austin where I acquired a diverse skill set in programming languages and methodologies.</p><br />
+
+                    <p>I am actively seeking opportunities that allow me to merge my mathematical knowledge and programming skills. My strong analytical mindset and problem-solving abilities make me well-suited for collaborative environments that demand attention to detail.
+
+                    Let's connect and discuss how we can make a positive impact together.</p>
+            </div>
         </div>
     )
 }
