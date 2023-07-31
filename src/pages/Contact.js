8347f9479a0caac55518f612f4c6258e4b73e6ee
Joaquin Salazar <joaqsala@gmail.com>
2023-07-30 23:01:08 -0500

    added contents of form in contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,8 +4,45 @@ import React from 'react';
 export default function Contact() {
     return (
         <div>
-            <h1>Contact Page</h1>
-            <p> This will need to include a form</p>
+            <h1 className="text-2xl mb-10">Contact Page</h1>
+            <section className="py-6 bg-gray-800 text-gray-50">
+                <div className="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
+                    <div className="py-6 md:py-0 md:px-6">
+                        <h1 className="text-2xl mb-4">Get in touch</h1>
+                        <p className="pt-2 pb-4">Fill in the form to start a conversation</p>
+                        <div className="space-y-4">
+                            <p className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                    <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd"></path>
+                                </svg>
+                                <span>Austin, TX  78747</span>
+                            </p>
+                            <p className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                                </svg>
+                                <span>joaqsala@gmail.com</span>
+                            </p>
+                        </div>
+                    </div>
+                    <form novalidate="" className="flex flex-col py-6 space-y-6 md:py-0 md:px-6">
+                        <label className="block">
+                            <span className="mb-1">Full name:</span>
+                            <input type="text" placeholder="Enter name" className="block p-1 w-full shadow-sm focus:ring focus:ri focus:ri bg-white text text-black"/>
+                        </label>
+                        <label className="block">
+                            <span className="mb-1">Email address:</span>
+                            <input type="email" placeholder="Enter email" className="block p-1 w-full shadow-sm focus:ring focus:ri focus:ri bg-white text text-black" />
+                        </label>
+                        <label className="block">
+                            <span className="mb-1">Message:</span>
+                            <textarea rows="5" className="block p-1 w-full focus:ring focus:ri focus:ri bg-white text text-black"></textarea>
+                        </label>
+                        <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ri bg-violet-400 text-gray-900 focus:ri hover:ri">Submit</button>
+                    </form>
+                </div>
+            </section>
         </div>
     )
 }
