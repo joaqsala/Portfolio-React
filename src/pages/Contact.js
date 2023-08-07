6dec2a477533e632e65939622cdd679cc869d9a9
Joaquin Salazar <joaqsala@gmail.com>
2023-08-06 21:08:16 -0500

    added formspree to handle emails from contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
 export default function Contact() {
     const [visitorName, setVisitorName] = useState("");
@@ -8,9 +9,10 @@ export default function Contact() {
     const [emailError, setEmailError] = useState("");
     const [messageError, setMessageError] = useState("");
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const [state, handleSubmit] = useForm("xjvqgnpb");
 
     const handleBlurName = () => {
-        if (visitorName.trim() ===""){
+        if (visitorName.trim() === "") {
             setNameError("Name cannot be left blank.");
         } else {
             setNameError("");
@@ -31,7 +33,6 @@ export default function Contact() {
         if (visitorMessage.trim() === "") {
             setMessageError("Message cannot be left blank.");
         } else {
-
             setMessageError("");
         }
     };
@@ -39,14 +40,17 @@ export default function Contact() {
     const handleSubmitMessage = async (e) => {
         e.preventDefault();
 
-          // Check for email validation error
-  if (emailError) {
-    alert("Invalid email format. Please enter a valid email.");
-    return; // Return early to prevent form submission
-  }
+        // Check for email validation error
+        if (emailError) {
+            alert("Invalid email format. Please enter a valid email.");
+            return; // Return early to prevent form submission
+        }
 
         if (visitorName && visitorEmail && visitorMessage) {
-            alert(`Cannot send at this time. \nPlease message directly at joaqsala@gmail.com.`)
+
+            // Formspree submission
+            handleSubmit(e);
+            // clear the form
             setVisitorName("");
             setVisitorEmail("");
             setVisitorMessage("");
@@ -70,6 +74,12 @@ export default function Contact() {
                                 </svg>
                                 <span>Austin, TX  78747</span>
                             </p>
+                            {/* <p className="flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
+                                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
+                                </svg>
+                                <span>123456789</span>
+                            </p> */}
                             <p className="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 mr-2 sm:mr-6">
                                     <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
@@ -79,30 +89,30 @@ export default function Contact() {
                             </p>
                         </div>
                     </div>
-                    <form className="flex flex-col py-6 space-y-6 md:py-0 md:px-6">
+                    <form className="flex flex-col py-6 space-y-6 md:py-0 md:px-6" onSubmit={handleSubmitMessage}>
                         <label className="block">
                             <span className="mb-1">Full name:</span>
-                            <input type="text" placeholder="Enter name" id="name" value={visitorName} className="block p-1 w-full shadow-sm focus:ring focus:ri focus:ri bg-white text text-black"
-                                onChange={(e) => setVisitorName(e.target.value)} 
-                                onBlur={handleBlurName} required/>
-                                {nameError && <div className="error text-red-600">{nameError}</div>}
+                            <input type="text" placeholder="Enter name" id="name" name="name" value={visitorName} className="block p-1 w-full shadow-sm focus:ring focus:ri focus:ri bg-white text text-black"
+                                onChange={(e) => setVisitorName(e.target.value)}
+                                onBlur={handleBlurName} required />
+                            {nameError && <div className="error text-red-600">{nameError}</div>}
                         </label>
                         <label className="block">
                             <span className="mb-1">Email address:</span>
-                            <input type="email" placeholder="Enter email" id="email" value={visitorEmail} className="block p-1 w-full shadow-sm focus:ring focus:ri focus:ri bg-white text text-black"
-                                onChange={(e) => setVisitorEmail(e.target.value)} 
-                                onBlur={handleBlurEmail} required/>
-                                {emailError && <div className="error  text-red-600">{emailError}</div>}
+                            <input type="email" placeholder="Enter email" id="email" name="email" value={visitorEmail} className="block p-1 w-full shadow-sm focus:ring focus:ri focus:ri bg-white text text-black"
+                                onChange={(e) => setVisitorEmail(e.target.value)}
+                                onBlur={handleBlurEmail} required />
+                            {emailError && <div className="error  text-red-600">{emailError}</div>}
                         </label>
                         <label className="block">
                             <span className="mb-1">Message:</span>
-                            <textarea rows="5" value={visitorMessage} id="message" className="block p-1 w-full focus:ring focus:ri focus:ri bg-white text text-black"
+                            <textarea rows="5" type="text" value={visitorMessage} id="message" name="message" className="block p-1 w-full focus:ring focus:ri focus:ri bg-white text text-black"
                                 onChange={(e) => setVisitorMessage(e.target.value)}
                                 onBlur={handleBlurMessage} required></textarea>
-                                {messageError && <div className="error text-red-600">{messageError}</div>}
+                            {messageError && <div className="error text-red-600">{messageError}</div>}
                         </label>
-                        <button type="button" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ri bg-violet-400 text-gray-900 focus:ri hover:ri"
-                            onClick={handleSubmitMessage}>Submit</button>
+                        <button type="submit" className="self-center px-8 py-3 text-lg rounded focus:ring hover:ring focus:ri bg-violet-400 text-gray-900 focus:ri hover:ri"
+                            disabled={state.submitting}>Submit</button>
                     </form>
                 </div>
             </section>
